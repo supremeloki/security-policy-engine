48dd4ae14f6eb96ae50306ab04ae9b25c2163422
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2025-08-15 21:26:18 +0330

    cover error branches pt2

--- tests/test_core.py
+++ tests/test_core.py
@@ -109,3 +109,4 @@ def test_flag_condition(engine):
         rule_id="break-glass", effect=Effect.ALLOW,
         actions=("emergency-write",), resources=("*",),
         environment_condition=env_flag_enabled("incident_active"),
+    ))
